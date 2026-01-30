abc1f91f3bffa03c530e51032ce73e60c8f3ce1b
DiegoMicali <diego.micali13@gmail.com>
2026-01-30 16:22:07 -0300

    feat: create MissoesController rotes with mock

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/Controller/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/Controller/MissoesController.java
@@ -1,4 +1,32 @@
 package dev.java10x.CadastroDeNinjas.Missoes.Controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
+
+    // GET -- Requisição para listar missões
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return "Missoes listadas com sucesso";
+    }
+
+    // PUT -- Requisição para criar missões
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "missão criada com sucesso";
+    }
+
+    // PUT -- Requisição para atualizar missões
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "missão alterada com sucesso";
+    }
+
+    // DELETE -- Requisição para deletar missões
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "missão deletada com sucesso";
+    }
 }
