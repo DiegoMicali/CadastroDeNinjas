7d87b6b44658339cea57ea7c7cfb329042b25add
DiegoMicali <diego.micali13@gmail.com>
2026-02-04 15:44:52 -0300

    feat: configure MissoesDTO and MissoesMapper

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/Mapper/MissoesMapper.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/Mapper/MissoesMapper.java
@@ -1,4 +1,33 @@
 package dev.java10x.CadastroDeNinjas.Missoes.Mapper;
 
+import dev.java10x.CadastroDeNinjas.Missoes.Service.MissoesDTO;
+import dev.java10x.CadastroDeNinjas.Missoes.Service.MissoesModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MissoesMapper {
+
+    public MissoesModel map(MissoesDTO missoesDTO) {
+
+        MissoesModel missoesModel = new MissoesModel();
+        missoesModel.setId(missoesDTO.getId());
+        missoesModel.setNome(missoesDTO.getNome());
+        missoesModel.setRank(missoesDTO.getRank());
+        missoesModel.setNinjas(missoesDTO.getNinjas());
+
+        return missoesModel;
+    }
+
+    public MissoesDTO map(MissoesModel missoesModel) {
+
+        MissoesDTO missoesDTO = new MissoesDTO();
+        missoesDTO.setId(missoesModel.getId());
+        missoesDTO.setNome(missoesModel.getNome());
+        missoesDTO.setRank(missoesModel.getRank());
+        missoesDTO.setNinjas(missoesModel.getNinjas());
+
+        return missoesDTO;
+    }
+
+
 }
