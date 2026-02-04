cad6832140a0b4b02c1a2cfc675ebe51ab9681c4
DiegoMicali <diego.micali13@gmail.com>
2026-02-04 13:57:32 -0300

    chore: move Mapper package to Ninja package and add manual mapping for entity x dto"

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Mapper/NinjaMapper.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Mapper/NinjaMapper.java
@@ -1,4 +1,37 @@
-package dev.java10x.CadastroDeNinjas.Mapper;
+package dev.java10x.CadastroDeNinjas.Ninjas.Mapper;
 
+import dev.java10x.CadastroDeNinjas.Ninjas.Service.NinjaDTO;
+import dev.java10x.CadastroDeNinjas.Ninjas.Service.NinjaModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO) {
+
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setImgUrl(ninjaDTO.getImgUrl());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel) {
+
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaDTO.getId());
+        ninjaDTO.setNome(ninjaDTO.getNome());
+        ninjaDTO.setEmail(ninjaDTO.getEmail());
+        ninjaDTO.setImgUrl(ninjaDTO.getImgUrl());
+        ninjaDTO.setIdade(ninjaDTO.getIdade());
+        ninjaDTO.setRank(ninjaDTO.getRank());
+        ninjaDTO.setMissoes(ninjaDTO.getMissoes());
+
+        return ninjaDTO;
+    }
 }
